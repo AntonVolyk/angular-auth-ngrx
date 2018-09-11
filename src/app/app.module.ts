fc0c69e2ff40f440eab71ad9a143abfb4bb60989
Volyk <GEDC_AVolyk@gedc.luxoft.com>
2018-09-11 16:45:43 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {SignUpComponent} from './components/sign-up/sign-up.component';
+import {LogInComponent} from './components/log-in/log-in.component';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from './services/auth.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'log-in', component: LogInComponent},
+      {path: 'sign-up', component: SignUpComponent},
+      {path: '', component: LandingComponent},
+      {path: '**', redirectTo: '/'}
+    ])
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
